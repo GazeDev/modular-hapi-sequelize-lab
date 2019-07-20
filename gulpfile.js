a1314e370381458822f95f71ad10cc54acfeb4f9
zaclittleberry <zlittleberry@linuxfoundation.org>
2019-07-20 16:49:53 -0400

    bring repo in line with current projects. start gitlab ci

--- gulpfile.js
+++ gulpfile.js
@@ -6,16 +6,22 @@ const modules = require('./lib/modules.js');
 
 gulp.task('test', function () {
   let argArray = [
+    '--globals', 'core,__core-js_shared__,@@any-promise/REGISTRATION',
     '--verbose',
     '--assert',
-    'code',
+    '@hapi/code',
   ];
   // build the files to test
   for (let mod of modules) {
     argArray.push(`lib/${mod}/${mod}.tests.js`);
   }
-  return gulp.src('test')
-    .pipe(lab(argArray));
+  return gulp.src('test', { allowEmpty: true })
+    .pipe(lab({
+      args: argArray,
+      opts: {
+        emitLabError: true
+      }
+    }));
 });
 
-gulp.task('default', ['test']);
+gulp.task('default', gulp.parallel('test'));
